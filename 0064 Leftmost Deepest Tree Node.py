85c63d1a7235b8d4a6a936658a5f8a560c0aa01b
Cindy Zhang <58195295+cindyxinyuzhang@users.noreply.github.com>
2021-11-07 22:59:41 -0500

    add 0064

---
+++ 0064 Leftmost Deepest Tree Node.py
@@ -0,0 +1,18 @@
+# class Tree:
+#     def __init__(self, val, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def solve(self, root):
+        if not root: return 0
+
+        queue = deque([root])
+
+        while queue:
+            for _ in range(len(queue)):
+                curr_node = queue.popleft()
+                if curr_node.right: queue.append(curr_node.right)
+                if curr_node.left: queue.append(curr_node.left)
+
+        return curr_node.val
