cd0d9cb6806ff439f7d043278d4cf930d8e7f8c4
Cindy Zhang <58195295+cindyxinyuzhang@users.noreply.github.com>
2021-11-16 17:02:07 -0500

    update 0149

--- 0149 No New Friends.py
+++ 0149 No New Friends.py
@@ -1,7 +1,3 @@
 class Solution:
     def solve(self, n, friends):
-        people_set = set()
-        for friend1, friend2 in friends:
-            people_set.add(friend1)
-            people_set.add(friend2)
-        return len(people_set) == n
+        return len({person for pair in friends for person in pair}) == n
