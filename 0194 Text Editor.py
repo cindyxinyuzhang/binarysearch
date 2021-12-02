551f202fb50ec0e4c568529667cbba1196345417
Cindy Zhang <58195295+cindyxinyuzhang@users.noreply.github.com>
2021-12-02 13:18:09 -0500

    add alternative 0194

--- 0194 Text Editor.py
+++ 0194 Text Editor.py
@@ -8,3 +8,16 @@ class Solution:
             else:
                 stack.append(char)
         return  ''.join(stack)
+
+class Solution:
+    def solve(self, s):
+        stack = []
+        i = 0
+        while i < len(s):
+            if i+1 < len(s) and s[i:i+2] == '<-':
+                if stack: stack.pop()
+                i += 1
+            else:
+                stack.append(s[i])
+            i += 1
+        return  ''.join(stack)
